cb6e3a72ab95018fd043d00b59d73596d8a35226
Oliver Atherton <118294630+olivrrcode@users.noreply.github.com>
2026-06-24 12:22:10 +0100

    oui oui mon ami

--- src/components/about/aboutBio.jsx
+++ src/components/about/aboutBio.jsx
@@ -1,4 +1,4 @@
-import Icon from "../icon";
+import Icon from "../icon/Icon";
 
 export default function AboutBio({ paragraphs, cvHref }) {
   return (
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,6 @@
 import "./projects.css";
 import PageSection from "../pageSection";
-import Icon from "../icon";
+import Icon from "../icon/Icon";
 
 const defaultProjects = [
   {
